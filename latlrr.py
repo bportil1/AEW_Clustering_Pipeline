37cf0b84705e0627d943b4586eb0fd109e41c87e
Bryan Portillo <karkaras@pop-os.msu.montana.edu>
2025-02-05 09:54:21 -0700

    renamed:    latrr_decomp.py -> latlrr.py

--- latlrr.py
+++ latlrr.py
@@ -12,7 +12,7 @@ class latLRR():
         self.y1 = 0
         self.y2 = 0
         self.y3 = 0
-        self.mew = 10e-6 # reg term
+        self.mu = 10e-6 # reg term
         self.max_u = 10**6
         self.rho = 1.1  # frob norm term  
         self.epsilon = 10e-6
@@ -31,6 +31,8 @@ class latLRR():
             if self.convergence_check():
                 converged = True
 
+        
+
     def schatten_norm_1(self, matrix):
         singular_vals = np.linalg.svd(matrix, compute_ux = False)
         return np.sum(singular_vals)
@@ -45,43 +47,43 @@ class latLRR():
         return U @ np.diag(S_thresholded) @ Vt
 
     def update_J(self):
-        tau = self.mew / self.rho
-        A = self.Z + (self.y2 / self.mew)
+        tau = self.mu / self.rho
+        A = self.Z + (self.y2 / self.mu)
         self.J = self.sv_thresholding(A, tau)
 
     def update_S(self):
         tau = self.mew / self.rho
-        A = self.L + (self.y3 / self.mew)
+        A = self.L + (self.y3 / self.mu)
         self.S = self.sv_thresholding(A, tau)
         
     def update_Z(self):
         term_1 = np.linalg.inv(self.identity + (self.matrix.T * self.matrix))
         term_2 = self.matrix.T * (self.matrix - (self.L * self.matrix) - self.E)
-        term_3 = ((self.matrix.T * self.y1) - self.y2) / self.mew
+        term_3 = ((self.matrix.T * self.y1) - self.y2) / self.mu
         self.Z = (term_1 * term_2) + self.J + term_3
 
     def update_L(self):
         term_1 = (self.matrix - (self.matrix * self.Z) - self.E) * self.matrix.T
-        term_2 = ((self.y1*self.matrix.T) - self.y3) / self.mew
+        term_2 = ((self.y1*self.matrix.T) - self.y3) / self.mu
         term_3 = np.linalg.inv(self.identity + (self.matrix * self.matrix.T))
         self.L = (term_1 + self.S + term_2) * term_3
 
     def update_E(self):
-        tau = self.mew / self.rho
-        term_1 = (self.matrix - self.matrix @ self.Z - self.L + self.y1) / self.mew
+        tau = self.mu / self.rho
+        term_1 = (self.matrix - self.matrix @ self.Z - self.L + self.y1) / self.mu
         self.E = np.sign(matrix) * np.maximum(np.abs(matrix) - tau, 0)
 
     def update_y1(self):
-        self.y1 += self.mew * (self.matrix - (self.matrix * self.Z) - (self.L * self.matrix) - self.E )
+        self.y1 += self.mu * (self.matrix - (self.matrix * self.Z) - (self.L * self.matrix) - self.E )
 
     def update_y2(self):
-        self.y2 += self.mew * (self.Z - self.J)
+        self.y2 += self.mu * (self.Z - self.J)
 
     def update_y3(self):
-        self.y3 += self.mew * (self.L - self.S)
+        self.y3 += self.mu * (self.L - self.S)
 
     def update_mew(self):
-        self.mew = np.min((self.rho*self.mew), self.max_u)
+        self.mu = np.min((self.rho*self.mu), self.max_u)
 
     def convergence_check(self):
         test1_term = self.matrix - (self.matrix * self.Z) - (self.L * self.matrix) - self.E
@@ -91,10 +93,10 @@ class latLRR():
         test2 = self.schatten_norm_inf(test2_term) < self.epsilon
 
         test3_term = self.L - self.S
-        test3 = self.schatten_norm_ing(test3_term) < self.epsilon
+        test3 = self.schatten_norm_inf(test3_term) < self.epsilon
 
         if test1 and test2 and test3:
-            return Tru
+            return True
         else:
             return False
 
