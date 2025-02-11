e4296b9e259e0629e590dfb9933e9bb703151296
Bryan Portillo <bryanportillo49@yahoo.com>
2025-02-11 09:26:57 -0700

    modified:   latlrr.py

--- latlrr.py
+++ latlrr.py
@@ -49,55 +49,76 @@ class latLRR():
     def svt(self, matrix):
         U, S, Vt = np.linalg.svd(matrix, full_matrices=False)
         tau = S[0] / self.rho
-        print("Current Tau: ", tau)
-        print("S init: ", S)
-        print("S Length: ", len(S))
+        #print("Current Tau: ", tau)
+        #print("S init: ", S)
+        #print("S Length: ", len(S))
         S_thresholded = np.maximum(S - tau, 0)
-        print("S threshed: ", S_thresholded)
-        print("S Threshed Size: ", len(S_thresholded))
+        #print("S threshed: ", S_thresholded)
+        #print("S Threshed Size: ", len(S_thresholded))
         return U @ np.diag(S_thresholded) @ Vt
 
-    def svt_optimization(self, matrix, max_iter, delta=1.2, l=5, tol=10e-4):
-        s_matrix = sparse.csr_matrix(matrix)
+    def orth_projection(self, matrix, indices):
+        proj_x = np.zeros_like(matrix)
+        for i, j in indices:
+            proj_x[i, j] = matrix[i, j]
+
+    def svt_optimization(self, matrix, max_iter, delta=1.2, l=1, tol=10e-4):
         Y = np.zeros_like(matrix)
         r = 0
-        tau = np.linalg.norm(matrix, "fro") * delta * 5
-        sv_count_condition = False
-        for k in range(1, max_iter):
-            s_k = r + 1
-            while (not sv_count_condition):
-                U, S, Vt = sparse.linalg.svds(k=s_k)
-                s_k = s_k + l
-                if S[s_k-l:k-1] <= tau:
-                    sv_count_condition = True
-            selected_sv = S[:r]
-            r = len(np.where(S > tau))
-            X = np.zeros_like(Y)
-            for idx in range(0,r):
-                sub_s = np.where(S > tau)
-                sub_s -= tau
-                sub_u = U[:r]
-                sub_v = Vt[:r]
-                X += sub_s @ sub_u @ sub_v  
+        opt_X = np.zeros_like(matrix)
+        #tau = np.minimum(len(matrix), len(matrix[0])) * 5
+        tau = np.average(np.linalg.svd(matrix, compute_uv=False))
+        print("Tau: ", tau)
+        for k in range(1, max_iter+1):
+            s = r + 1
+            #while True:
+            #print("Y: ", Y)
+            #print("Y: ", np.all(Y==0))
+            if np.all(Y==0):
+                Y = Y + delta * (matrix)
+                continue
+            while True:
+                U, S, Vt = np.linalg.svd(Y, full_matrices=False)
+                #print("U: ", U)
+                #print("S: ", S)
+                #print("Vt: ", Vt)
 
-            if np.linalg.norm((X - matrix), 'fro') / np.linalg.norm((
+                if s - 1 < len(S) and S[s - l - 1]  <= tau:
+                    break
+                s += 1
+                    
+            r = max(j for j in range(len(S)) if S[j] > tau)
+            curr_X = sum((S[j] - tau) * np.outer(U[:, j], Vt[j, :]) for j in range(r))
 
-        return 0 
+            print("Current Convergence Condition: ", np.linalg.norm(curr_X - matrix, 'fro') /np.linalg.norm(matrix, 'fro'))
+
+            if np.linalg.norm(curr_X - matrix, 'fro') /np.linalg.norm(matrix, 'fro') <= tol:
+                opt_X = curr_X
+                break
+
+            Y = Y + delta * (matrix - curr_X)
+            
+        return opt_X 
 
     def update_J(self):
         print("In update J")
         #tau = self.mu / self.rho
         #print("Curr Tau: ", tau)
         A = self.J - (self.Z + (self.y2 / self.mu))
-        self.J = self.svt(A)
-        print("New J: ", self.J)
+        #print("A: ", A)
+        #self.J = self.svt(A)
+        if (A.all() != 0):
+            self.J = self.svt_optimization(A, 1000)
+        else: 
+            self.J = A
+        #print("New J: ", self.J)
 
     def update_S(self):
         print("In update S")
         #tau = self.mu / self.rho
         A = self.S - (self.L + (self.y3 / self.mu))
         self.S = self.svt(A)
-        print("New S: ", self.S)
+        #print("New S: ", self.S)
 
     def update_Z(self):
         term_1 = np.linalg.inv(self.identity_row + np.dot(self.matrix.T, self.matrix))
@@ -108,7 +129,7 @@ class latLRR():
         #print("term2 shape: ", term_2.shape)
         #print("term3 shape: ", term_3.shape)
         self.Z = np.dot(term_1, (term_2 + self.J + term_3))
-        print("New Z: ", self.Z)
+        #print("New Z: ", self.Z)
 
     def update_L(self):
         term_1 = np.dot((self.matrix - np.dot(self.matrix, self.Z) - self.E), self.matrix.T)
@@ -116,7 +137,7 @@ class latLRR():
         term_3 = np.linalg.inv(self.identity_col + np.dot(self.matrix, self.matrix.T))
         print("In update L")
         self.L = np.dot((term_1 + self.S + term_2), term_3)
-        print("New L: ", self.L)
+        #print("New L: ", self.L)
 
     def update_E(self):
         print("In update E")
@@ -124,7 +145,7 @@ class latLRR():
         sigma_max = np.linalg.svd(term_1, compute_uv=False)[0]
         tau = sigma_max / self.rho
         self.E = (np.sign(term_1) * np.maximum(np.abs(term_1) - tau, 0))
-        print("New E: ", self.E)
+        #print("New E: ", self.E)
 
     def update_y1(self):
         self.y1 += self.mu * (self.matrix - np.dot(self.matrix, self.Z) - np.dot(self.L, self.matrix) - self.E )
@@ -136,11 +157,11 @@ class latLRR():
         self.y3 += self.mu * (self.L - self.S)
 
     def update_mu(self):
-        print("Current Mu: ", self.mu)
-        print("Current Rho: ", self.rho)
-        print("Current Max_u: ", self.max_u)
+        #print("Current Mu: ", self.mu)
+        #print("Current Rho: ", self.rho)
+        #print("Current Max_u: ", self.max_u)
         self.mu = np.minimum((self.rho*self.mu), self.max_u)
-        print("New Mu: ", self.mu)
+        #print("New Mu: ", self.mu)
 
     def convergence_check(self):
         test1_term = self.matrix - np.dot(self.matrix, self.Z) - np.dot(self.L, self.matrix) - self.E
