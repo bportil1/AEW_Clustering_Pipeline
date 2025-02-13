2079288cccc3731b506da6ef2ad8065fef31c362
Bryan Portillo <bryanportillo49@yahoo.com>
2025-02-13 06:38:17 -0700

    modified:   latlrr.py

--- latlrr.py
+++ latlrr.py
@@ -31,6 +31,16 @@ class latLRR():
             self.update_y2()
             self.update_y3()
             self.update_mu()
+            print("J: ", self.J)
+            print("S: ", self.S)
+            print("Z: ", self.Z)
+            print("L: ", self.L)
+            print("E: ", self.E)
+            print("Y1: ", self.y1)
+            print("Y2: ", self.y2)
+            print("Y3: ", self.y3)
+            print("MU: ", self.mu)
+
             if self.convergence_check():
                 converged = True
             print("Not Converged Continuing")
@@ -46,17 +56,6 @@ class latLRR():
         singular_vals = np.linalg.svd(matrix, compute_uv = False)
         return np.max(singular_vals)
 
-    def svt(self, matrix):
-        U, S, Vt = np.linalg.svd(matrix, full_matrices=False)
-        tau = S[0] / self.rho
-        #print("Current Tau: ", tau)
-        #print("S init: ", S)
-        #print("S Length: ", len(S))
-        S_thresholded = np.maximum(S - tau, 0)
-        #print("S threshed: ", S_thresholded)
-        #print("S Threshed Size: ", len(S_thresholded))
-        return U @ np.diag(S_thresholded) @ Vt
-
     def orth_projection(self, matrix, indices):
         proj_x = np.zeros_like(matrix)
         for i, j in indices:
@@ -66,14 +65,12 @@ class latLRR():
         Y = np.zeros_like(matrix)
         r = 0
         opt_X = np.zeros_like(matrix)
-        #tau = np.minimum(len(matrix), len(matrix[0])) * 5
         tau = np.average(np.linalg.svd(matrix, compute_uv=False))
-        print("Tau: ", tau)
+        #print("Tau: ", tau)
+        #print("Init Matrix: ", matrix)
+        #print("Init Rank:  ", np.linalg.matrix_rank(matrix))
         for k in range(1, max_iter+1):
             s = r + 1
-            #while True:
-            #print("Y: ", Y)
-            #print("Y: ", np.all(Y==0))
             if np.all(Y==0):
                 Y = Y + delta * (matrix)
                 continue
@@ -86,69 +83,119 @@ class latLRR():
                 if s - 1 < len(S) and S[s - l - 1]  <= tau:
                     break
                 s += 1
-                    
+            #print("Current S:  ", s)        
             r = max(j for j in range(len(S)) if S[j] > tau)
             curr_X = sum((S[j] - tau) * np.outer(U[:, j], Vt[j, :]) for j in range(r))
 
-            print("Current Convergence Condition: ", np.linalg.norm(curr_X - matrix, 'fro') /np.linalg.norm(matrix, 'fro'))
+            #print("Current Convergence Condition: ", np.linalg.norm(curr_X - matrix, 'fro') /np.linalg.norm(matrix, 'fro'))
 
             if np.linalg.norm(curr_X - matrix, 'fro') /np.linalg.norm(matrix, 'fro') <= tol:
                 opt_X = curr_X
                 break
 
             Y = Y + delta * (matrix - curr_X)
-            
+        #print("Reduced Matrix: ", opt_X)
+        #print("Reduced Matrix: ", opt_X.shape)
+        #print("Reduced Matrix Rank: ", np.linalg.matrix_rank(opt_X))
         return opt_X 
+
+    ######## check multiplication here #########
+    def soft_thresholding(self, matrix, tau):
+        return np.sign(matrix) * np.maximum(np.abs(matrix) - tau, 0)
+
+    def pg_optimization(self, matrix, max_iter, tol=10e-4):
+        #tau = np.average(np.linalg.svd(matrix, compute_uv=False))
+        tau = .9 / self.mu
+        curr_E = np.zeros_like(matrix)
+        
+        for k in range(max_iter):
+            last_E = np.copy(curr_E)
+            curr_E = self.soft_thresholding(matrix, tau) 
+            if np.linalg.norm(matrix - last_E, ord='fro') < tol:
+                break
+        return last_E
+        
 
     def update_J(self):
         print("In update J")
-        #tau = self.mu / self.rho
-        #print("Curr Tau: ", tau)
         A = self.J - (self.Z + (self.y2 / self.mu))
-        #print("A: ", A)
-        #self.J = self.svt(A)
         if (A.all() != 0):
             self.J = self.svt_optimization(A, 1000)
-        else: 
-            self.J = A
-        #print("New J: ", self.J)
+        #else: 
+        #    self.J = A
 
     def update_S(self):
         print("In update S")
-        #tau = self.mu / self.rho
         A = self.S - (self.L + (self.y3 / self.mu))
-        self.S = self.svt(A)
-        #print("New S: ", self.S)
+        if (A.all() != 0):
+            self.S = self.svt_optimization(A, 1000)
+        #else: 
+        #    self.S = A
 
     def update_Z(self):
-        term_1 = np.linalg.inv(self.identity_row + np.dot(self.matrix.T, self.matrix))
-        term_2 = np.dot(self.matrix.T, (self.matrix - np.dot(self.L, self.matrix) - self.E))
-        term_3 = (np.dot(self.matrix.T, self.y1) - self.y2) / self.mu
+        #term_1 = np.linalg.inv(self.identity_row + np.dot(self.matrix.T, self.matrix))
+        term_1 = self.identity_row + np.matmul(self.matrix.T, self.matrix)
+
+        term_2 = np.matmul(self.matrix.T, (self.matrix - np.matmul(self.L, self.matrix) - self.E))
+        term_3 = (np.matmul(self.matrix.T, self.y1) - self.y2) / self.mu
         print("In update_Z")
         #print("term1 shape: ", term_1.shape)
         #print("term2 shape: ", term_2.shape)
         #print("term3 shape: ", term_3.shape)
-        self.Z = np.dot(term_1, (term_2 + self.J + term_3))
+        #self.Z = np.dot(term_1, (term_2 + self.J + term_3))
+        #self.Z = (term_2 + self.J + term_3) / term_1
+        self.Z = np.linalg.solve(term_1, (term_2 + self.J + term_3))
+
         #print("New Z: ", self.Z)
 
     def update_L(self):
-        term_1 = np.dot((self.matrix - np.dot(self.matrix, self.Z) - self.E), self.matrix.T)
-        term_2 = (np.dot(self.y1, self.matrix.T) - self.y3) / self.mu
-        term_3 = np.linalg.inv(self.identity_col + np.dot(self.matrix, self.matrix.T))
         print("In update L")
-        self.L = np.dot((term_1 + self.S + term_2), term_3)
+        #reg_matrix = 1e-6 * np.eye(self.matrix.shape[0])
+        term_1 = np.matmul((self.matrix - np.matmul(self.matrix, self.Z) - self.E), self.matrix.T)
+        term_2 = (np.matmul(self.y1, self.matrix.T) - self.y3) / self.mu
+        term_3 = np.linalg.pinv(self.identity_col + np.matmul(self.matrix, self.matrix.T))
+        
+        #term_3 = self.identity_col + np.matmul(self.matrix, self.matrix.T) 
+        #term_3 = np.linalg.solve(term_3, np.eye(term_3.shape[0]))
+        #self.L = np.matmul((term_1 + self.S + term_2), term_3)
+        
+        self.L = np.matmul((term_1 + self.S + term_2), term_3)
+
         #print("New L: ", self.L)
 
     def update_E(self):
         print("In update E")
-        term_1 = self.E - ((self.matrix - np.dot(self.matrix, self.Z) - np.dot(self.L, self.matrix) + self.y1) / self.mu)
-        sigma_max = np.linalg.svd(term_1, compute_uv=False)[0]
-        tau = sigma_max / self.rho
-        self.E = (np.sign(term_1) * np.maximum(np.abs(term_1) - tau, 0))
-        #print("New E: ", self.E)
+
+        #print("J: ", self.J)
+        #print("S: ", self.S)
+        #print("Z: ", self.Z)
+        #print("L: ", self.L)
+        #print("E: ", self.E)
+        #print("Y1: ", self.y1)
+        #print("Y2: ", self.y2)
+        #print("Y3: ", self.y3)
+        #print("MU: ", self.mu)
+
+        print("first matmul: ", np.matmul(self.matrix, self.Z) )
+        print("second matmul: ", np.matmul(self.L, self.matrix)) 
+
+        print("matrix: ", self.matrix - np.matmul(self.matrix, self.Z) - np.matmul(self.L, self.matrix))
+
+        A = self.E - ((self.matrix - np.matmul(self.matrix, self.Z) - np.matmul(self.L, self.matrix) + self.y1) / self.mu)
+        
+        print("A: ", A)
+
+        if (A.all() != 0):
+            self.E = self.pg_optimization(A, 1000)
+        else:
+            self.E = A
+        #sigma_max = np.linalg.svd(term_1, compute_uv=False)[0]
+        #tau = sigma_max / self.rho
+        #self.E = (np.sign(term_1) * np.maximum(np.abs(term_1) - tau, 0))
+        print("New E: ", self.E)
 
     def update_y1(self):
-        self.y1 += self.mu * (self.matrix - np.dot(self.matrix, self.Z) - np.dot(self.L, self.matrix) - self.E )
+        self.y1 += self.mu * (self.matrix - np.matmul(self.matrix, self.Z) - np.matmul(self.L, self.matrix) - self.E )
 
     def update_y2(self):
         self.y2 += self.mu * (self.Z - self.J)
@@ -157,18 +204,16 @@ class latLRR():
         self.y3 += self.mu * (self.L - self.S)
 
     def update_mu(self):
-        #print("Current Mu: ", self.mu)
-        #print("Current Rho: ", self.rho)
-        #print("Current Max_u: ", self.max_u)
         self.mu = np.minimum((self.rho*self.mu), self.max_u)
         #print("New Mu: ", self.mu)
 
     def convergence_check(self):
-        test1_term = self.matrix - np.dot(self.matrix, self.Z) - np.dot(self.L, self.matrix) - self.E
+        test1_term = self.matrix - np.matmul(self.matrix, self.Z) - np.matmul(self.L, self.matrix) - self.E
         test1_res = self.schatten_norm_inf(test1_term)
         test1 = test1_res < self.epsilon
 
         print("Current Epsilon: ", self.epsilon)
+        print("Current Mu: ", self.mu)
 
         print("Test 1 Value: ", test1_res)
 
